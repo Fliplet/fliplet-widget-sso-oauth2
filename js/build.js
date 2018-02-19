59d4e054f36b21b8903aec7aeca19260e721eb76
Nicholas Valbusa <squallstar@gmail.com>
2018-02-19 11:12:11 +0100

    tweak

--- js/build.js
+++ js/build.js
@@ -1,18 +1,18 @@
-Fliplet.Widget.register('com.fliplet.sso.saml2', function registerComponent() {
+Fliplet.Widget.register('com.fliplet.sso.oauth2', function registerComponent() {
   return {
     authorize: function(opts) {
       return new Promise(function(resolve, reject) {
         Fliplet.Navigate.to({
           action: 'url',
           inAppBrowser: true,
-          url: Fliplet.Env.get('apiUrl') + 'v1/session/authorize/saml2?appId=' + Fliplet.Env.get('appId') + '&auth_token=' + Fliplet.User.getAuthToken(),
+          url: Fliplet.Env.get('apiUrl') + 'v1/session/authorize/oauth2?appId=' + Fliplet.Env.get('appId') + '&auth_token=' + Fliplet.User.getAuthToken(),
           // onmessage: function(message) {
           //   // resolve when authentication flow is done
           //   resolve(message);
           // },
           onclose: function() {
             Fliplet.Session.get().then(function(session) {
-              if (session.server.passports.saml2) {
+              if (session.server.passports.oauth2) {
                 return resolve();
               }
 
